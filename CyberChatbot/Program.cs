f2541ebcdf686b6ff5ae4e50ab283ae6db00fa87
Mulamu25 <st10498756@rcconnect.edu.za>
2026-08-24 16:39:41 -0500

    Added personalized welcome message

--- CyberChatbot/Program.cs
+++ CyberChatbot/Program.cs
@@ -10,6 +10,7 @@ namespace CyberChatbot
             AudioPlayer audio = new AudioPlayer();
             audio.PlayWelcomeMessage();
 
+
             Console.ForegroundColor = ConsoleColor.Cyan;
            
             Console.WriteLine(@"  
@@ -23,12 +24,25 @@ namespace CyberChatbot
          ▀▀▀                                                            ▀▀▀                                                                                        
 =============================================================================================================================================================
 ");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("      WELCOME TO THE CYBERCHATBOT SYSTEM");
+            Console.WriteLine("===================================================================");
 
 
-       
+            ResponseHandler handler = new ResponseHandler();
+            
+            Console.Write($"Please enter your name: {handler.Name}");
+            handler.Name = Console.ReadLine();
+
+
+            Console.WriteLine($"\nWelcome, {handler.Name}! You are now connected to the CyberChatbot System.");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+
     }
 
     }
