4ced19b3a931543b8133128d0fced62c2e05e8e1
Mulamu25 <st10498756@rcconnect.edu.za>
2026-09-20 08:30:43 -0500

    Fix: Password case sensitivity, personalization, Readme formatting

--- CyberChatbot/ResponseHandler.cs
+++ CyberChatbot/ResponseHandler.cs
@@ -12,45 +12,45 @@ namespace CyberChatbot
 
         public void GetResponse(string input)
         {
-            
-                 string question = input.ToLower().Trim();
-               
 
-                if (question == "exit")
-                {
-                    Console.WriteLine("Goodbye! Stay safe online");
-                }
-                else if (question.Contains("how are you"))
-                {
-                    Console.WriteLine($"I am doing well and ready to help you, {Name}!.");
-                }
-                else if (question.Contains("purpose"))
-                {
-                    Console.WriteLine("The purpose is to assist you with cyber safety information and guidance.");
-                }
-                else if (question.Contains("What can i ask") || question.Contains("topic"))
-                {
-                    Console.WriteLine("You can ask about passwords, phishing and safe browsing.");
-                }
-                else if (question.Contains("passwords"))
-                {
-                    Console.WriteLine("Use strong, unique passwords for each account and consider using a password manager.");
-                }
-                else if (question.Contains("phishing"))
-                {
-                    Console.WriteLine("Be cautious of suspicious emails or messages asking for personal information. Verify the source before clicking any links.");
-                }
-                else if (question.Contains("safe browsing"))
-                {
-                    Console.WriteLine("Use secure websites (https), avoid clicking on unknown links, and keep your browser updated.");
-                }
-                else
-                {
-                    Console.WriteLine("I didnt quite understand that. Could you  rephrase");
-                }
+            string question = input.ToLower().Trim();
 
-            } 
+
+            if (question == "exit")
+            {
+                Console.WriteLine("Goodbye! Stay safe online");
+            }
+            else if (question.Contains("how are you"))
+            {
+                Console.WriteLine($"I am doing well and ready to help you, {Name}!.");
+            }
+            else if (question.Contains("purpose"))
+            {
+                Console.WriteLine($"The purpose is to assist you with cyber safety information and guidance, {Name}!.");
+            }
+            else if (question.Contains("what can i ask") || question.Contains("topic"))
+            {
+                Console.WriteLine($"You can ask about passwords, phishing and safe browsing, {Name}!.");
+            }
+            else if (question.Contains("password"))
+            {
+                Console.WriteLine($"Use strong, unique passwords for each account and consider using a password manager, {Name}!.");
+            }
+            else if (question.Contains("phishing"))
+            {
+                Console.WriteLine($"Be cautious of suspicious emails or messages asking for personal information. Verify the source before clicking any links, {Name}!.");
+            }
+            else if (question.Contains("safe browsing"))
+            {
+                Console.WriteLine($"Use secure websites (https), avoid clicking on unknown links, and keep your browser updated, {Name}!.");
+            }
+            else
+            {
+                Console.WriteLine($"I didnt quite understand that. Could you  rephrase, {Name}?.");
+            }
+
         }
     }
+}
 
 
